9ddbf4a44eb4f233ad9852ae560428f2a467592c
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-08-30 20:30:20 -0300

    Adicionado exercicios de fixacao

---
+++ Fundamentos/Bloco.04 - Introducao a JavaScript e Logica de Programacao/dia-1-javascript-primeiros-passos/1. fixacao-4.1/tipo-tipagem-operacoes.js
@@ -0,0 +1,25 @@
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = area;
+
+console.log(perimeter);
---
+++ Fundamentos/Bloco.04 - Introducao a JavaScript e Logica de Programacao/dia-1-javascript-primeiros-passos/1. fixacao-4.1/variaveis.js
@@ -0,0 +1,6 @@
+const myName = 'Bruno Ribeiro';
+const birthCity = 'Belo Horizonte';
+let birthYear = 1986;
+
+birthYear = 2030;
+birthCity = 'Londres';
