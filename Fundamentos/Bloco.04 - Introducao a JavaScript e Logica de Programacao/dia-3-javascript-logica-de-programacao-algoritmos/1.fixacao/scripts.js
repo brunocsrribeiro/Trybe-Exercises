3f2226aaa7da11fc400a284aae2ca86e0508b211
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-06 21:08:53 -0300

    Adicionado exercicio 4.3

---
+++ Fundamentos/Bloco.04 - Introducao a JavaScript e Logica de Programacao/dia-3-javascript-logica-de-programacao-algoritmos/1.fixacao/scripts.js
@@ -0,0 +1,16 @@
+let linhaAsteriscos = 5;
+
+for (let index = 0; index < linhaAsteriscos; index += 1) {
+  let linhaAsteriscos2 = "";
+  for (let linha = 0; linha < linhaAsteriscos; linha +=1) {
+    linhaAsteriscos2 += "*"
+  }
+  console.log(linhaAsteriscos2);
+}
+
+let linhaAsteriscos2 = 5;
+let trianguloAsterisco = "";
+for (let index = 1; index <= linhaAsteriscos2; index += 1) {
+  trianguloAsterisco += "*";
+  console.log(trianguloAsterisco);
+}
