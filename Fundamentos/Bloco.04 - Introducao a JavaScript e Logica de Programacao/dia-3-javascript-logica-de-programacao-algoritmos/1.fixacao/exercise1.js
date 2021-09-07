6a1f6586f77c33be418b5901b1837d35213757e3
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-07 17:50:50 -0300

    Adicionado exercicio 1

---
+++ Fundamentos/Bloco.04 - Introducao a JavaScript e Logica de Programacao/dia-3-javascript-logica-de-programacao-algoritmos/1.fixacao/exercise1.js
@@ -0,0 +1,10 @@
+let lines = 5;
+let symbols = "*";
+let inputLine = "";
+
+for (let idx = 0; idx < lines; idx += 1) {
+  inputLine += symbols;
+}
+for (let lineIdx = 0; lineIdx < lines; lineIdx += 1) {
+  console.log(inputLine);
+}
