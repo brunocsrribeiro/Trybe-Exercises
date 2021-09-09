8b4e81df74b261ba0189543c42fa21caac25a460
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-09 10:43:50 -0300

    Refatorando exercicio 2 e explanando o exercicio 1

--- Fundamentos/Bloco.04 - Introducao a JavaScript e Logica de Programacao/dia-4-javascript-objetos-e-funcoes/Exercises/exercise2.js
+++ Fundamentos/Bloco.04 - Introducao a JavaScript e Logica de Programacao/dia-4-javascript-objetos-e-funcoes/Exercises/exercise2.js
@@ -1,4 +1,8 @@
 // 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+// SOLUÇÂO DO GABARITO
+// Utilizei a mesma lógica do gabarito e meu entendimento foi:
+ // Ao declarar uma variável (checked) na estrutura da função, que recebe a String da função utilizando o metódo .split('') com uma string vazia, que irá separar essa string por caracteres e retornar uma array, mais o metódo .reverse(), que irá inverter as posições deste array, onde o valor que está na primeira posição do index passa a ocupar a última posição e a última posição passa a ocupar a primeira. Após os metódos  separarem e inverterem as posições o .join(''), com uma string vazia vai unir todos os caracteres formando uma string que ocupará uma única posição dentro da array. Na sequência é utilizada a condicional if que irá comparar se há igualdade entre os valores. Caso a condição seja verdadeira, será retornado true, agora, caso condição seja falsa, ela entra no else e o retorno será false. 
 function palindromeCheck(animal) {
   let checked = animal.split('').reverse().join('');
   if (checked === animal) {
@@ -8,12 +12,28 @@ function palindromeCheck(animal) {
   }
 }
 
-console.log(palindromeCheck('arara'));
-console.log(palindromeCheck('desenvolvimento'));
+palindromeCheck('arara');
+palindromeCheck('desenvolvimento');
 
 // 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
-function highestValue(arrInt) {
-    return arrInt.indexOf(Math.max.apply(0, arrInt));
+
+// MINHA SOLUÇÃO
+function highestArr(arrInt) {
+  let highestValue = arrInt.indexOf(Math.max.apply(0, arrInt));
+  console.log(highestValue);
 }
 
-console.log(highestValue([2, 4, 6, 7, 10, 0, -3]));
+highestArr([2, 4, 6, 7, 10, 2, -3]);
+
+// SOLUÇÃO GABARITO
+function highestArray(arrInt) {
+  let highestValues = 0;
+  for (let idx in arrInt) {
+    if (arrInt[highestValues] < arrInt[idx]) {
+      highestValues = idx;
+    } 
+  }
+  console.log(highestValues);
+}
+
+highestArray([2, 4, 6, 7, 10, 2, -3]);
