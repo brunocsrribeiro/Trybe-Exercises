6142bc12ff1e77d374f7b07128e3553439783d3b
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-10-04 23:54:23 -0300

    Adiciona exercicio

--- Fundamentos/Bloco.08 - Higher order Functions do JavaScript ES6/dia-1-javascript-es6-introducao-a-higher-order-functions/Exercises/script.js
+++ Fundamentos/Bloco.08 - Higher order Functions do JavaScript ES6/dia-1-javascript-es6-introducao-a-higher-order-functions/Exercises/script.js
@@ -1,20 +1,12 @@
-// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
-const employeesRegistration = (fullName) => {
-  const emailEmployee = fullName.toLowerCase().split(' ').join('_');
-  return {
-    fullName,
-    email: `${emailEmployee}@trybe.com`,
-  };
-}
+const chosenNumber = (prizeDraw, myNumber) => prizeDraw === myNumber;
 
-const newEmployees = (callback) => {
-  const employees = {
-    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  }
-  return employees;
+const drawnNumber = (prizeDraw, callback) => {
+  const myNumber = Math.floor((Math.random() * 5) + 1);
+  
+  return callback(prizeDraw, myNumber) ? "Parabéns você ganhou" : "Tente novamente";
 };
 
-console.log(newEmployees(employeesRegistration));
+console.log(drawnNumber(2, chosenNumber));
+
