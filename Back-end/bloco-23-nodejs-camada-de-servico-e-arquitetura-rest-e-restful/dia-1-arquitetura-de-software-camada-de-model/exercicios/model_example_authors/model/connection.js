54a3ec9a2ddc4377b87ce6b9120f4bf201cd2665
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-04-01 23:10:08 -0300

    Delete connection.js

--- Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/model_example_authors/model/connection.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2/promise');
-
-module.exports = connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Br131908',
-  database: 'model_example'
-});
